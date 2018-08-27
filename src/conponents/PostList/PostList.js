251d78dbc5f08b3416d3a1a721b81e3eced32d0e
GzwNice <gzwnice@163.com>
2018-08-27 11:43:34 +0800

    创建PostItem组件，通过组件props来展示UI

--- src/conponents/PostList/PostList.js
+++ src/conponents/PostList/PostList.js
@@ -1,4 +1,11 @@
 import React, {Component} from 'react'
+import PostItem from '../PostItem/PostItem'
+
+const date =[
+  {title:'大家一起来讨论react吧', author:'张三', date:'2018-08-27 11:14'},
+  {title:'前端框架，你最喜欢哪一个', author:'李四', date:'2018-08-27 12:00'},
+  {title:'App的时代已经到来', author:'王五', date:'2018-08-27 18:30'},
+];
 
 class PlostList extends Component {
   render (){
@@ -6,9 +13,13 @@ class PlostList extends Component {
       <div>
         帖子列表：
         <ul>
-          <li>大家一起来讨论react吧</li>
-          <li>前端框架，你最喜欢哪一个</li>
-          <li>Web App的时代已经到来</li>
+          {date.map(item => 
+            <PostItem 
+             title= {item.title}
+             author= {item.author}
+             date= {item.date}
+            />
+          )}
         </ul>
       </div>
     );
