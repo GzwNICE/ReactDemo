d7ec930684b12bf31395a084b5cc68d477148498
GzwNice <gzwnice@163.com>
2018-09-11 14:52:18 +0800

    高阶组件不求甚解~~~

--- src/conponents/withPersistentData/withPersistentData.js
+++ src/conponents/withPersistentData/withPersistentData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'net';
 
 // 高阶组价的主要功能是封装并分离组件的通用逻辑，让通用逻辑在组件间更好的被复用
 // HOC(...params)()   高阶组件  
@@ -25,3 +26,8 @@ class MyComponent extends Component {
 const MyComponent1WithPersistentData = withPersistentData('data')(MyComponent)
 // 获取key = 'name' 的数据
 const MyComponent2WithPersistentData = withPersistentData('name')(MyComponent)
+
+//HOC(...params)()   高阶组件大量出现在第三方组件库中 
+//react-redux 中的connect函数
+connect (mapStateToProps,mapDispatchToProps)(WrappedComponent)
+// connect的参数mapStateToProps，mapDispatchToProps是函数类型，说明高组件的参数可以使函数类型
