1492f4f5644b083341bf452df365f28ba697371f
gzwnice <gzwnice@163.com>
2018-09-15 15:55:51 +0800

    Header组件完成

--- src/conponents/Header/Header.js
+++ src/conponents/Header/Header.js
@@ -3,12 +3,30 @@ import { Link } from 'react-router-dom'
 import styles from './style'
 
 export default class Header extends Component {
-
-    render() {
-        return (
-            <div>
-            
-            </div>
-        )
-    }
+  render() {
+    const { username, onLogout, location } = this.props
+    return (
+      <div className={styles.header}>
+        <div className={styles.nav}>
+          <span className={styles.leftLink}>
+            <Link to="/">首页</Link>
+          </span>
+          {username && username.length > 0 ? (
+            <span className={styles.user}>
+              当前用户：
+              {username}
+              &nbsp;
+              <button onClick={onLogout}>注销</button>
+            </span>
+          ) : (
+            <span className={styles.rightLink}>
+              <Link to={{ pathname: '/login', state: { from: location } }}>
+                登录
+              </Link>
+            </span>
+          )}
+        </div>
+      </div>
+    )
+  }
 }
