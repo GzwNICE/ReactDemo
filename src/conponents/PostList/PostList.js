998f3a1654ea1e7ecdd1e3becaa2a9291c3b2272
gzwnice <gzwnice@163.com>
2018-09-16 01:08:38 +0800

    PostList less

--- src/conponents/PostList/PostList.js
+++ src/conponents/PostList/PostList.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react'
+import styles from './style.less'
 
 export default class PostList extends Component {
   render() {
-    return (
-      <div>
-        PostList
-      </div>
-    )
+    return <div className={styles.box}>PostList</div>
   }
 }
