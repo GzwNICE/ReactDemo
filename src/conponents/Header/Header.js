d8d485718ead51f44568ec3724f2f89af9216bab
gzwnice <gzwnice@163.com>
2018-09-15 14:59:35 +0800

    header组件begin

---
+++ src/conponents/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import styles from './style'
+
+export default class Header extends Component {
+
+    render() {
+        return (
+            <div>
+            
+            </div>
+        )
+    }
+}
