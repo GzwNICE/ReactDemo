0e7cc94518ae089eed55cc6b369dd02d35dd31e7
GzwNice <gzwnice@163.com>
2018-10-12 10:04:03 +0800

    更新命名错误

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./conponents/Home/Home";
-import Login from "./conponents/Login/Login";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 
 export default class App extends Component {
   render() {
